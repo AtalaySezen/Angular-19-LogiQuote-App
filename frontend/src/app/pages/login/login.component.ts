bbccde897515af8a35962d6c41dd8afcdede83aa
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-12-03 22:11:12 +0300

    feats: add login page design and form validation - Created login form using Angular Reactive Forms - Added form validation for username and password fields - Integrated NG Zorro UI components (Button, Checkbox, Input, Form) - Fixed NG Zorro icons not loading by configuring assets in angular.json - Updated app configuration with HttpClient and BrowserAnimationsModule providers - Login form - Added email validation and error messages

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, NzButtonModule, RouterModule, NzCheckboxModule, NzFormModule, NzInputModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  formBuilder = inject(NonNullableFormBuilder);
 
+  loginForm = this.formBuilder.group({
+    email: this.formBuilder.control('', [Validators.required, Validators.email]),
+    password: this.formBuilder.control('', [Validators.required, Validators.minLength(3)]),
+    remember: this.formBuilder.control(true)
+  });
+
+  submitLoginForm(): void {
+    if (this.loginForm.valid) {
+      console.log('login', this.loginForm.value);
+    } else {
+      Object.values(this.loginForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
