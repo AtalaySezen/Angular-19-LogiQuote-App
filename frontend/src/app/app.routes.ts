6af102fa1be3960f1354a17729559c950ee112e7
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-12-04 16:00:09 +0300

    feat: add offer and offer-list components, update app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { OfferComponent } from './pages/offer/offer.component';
+import { OfferListComponent } from './pages/offer-list/offer-list.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent, title: 'Home' },
+    { path: 'offer', component: OfferComponent, title: 'Offer' },
+    { path: 'offer-list', component: OfferListComponent, title: 'Offer Lists' },
     { path: 'login', component: LoginComponent, title: 'Login' },
     { path: 'register', component: RegisterComponent, title: 'Register' },
 ];
--- frontend/src/app/pages/offer-list/offer-list.component.spec.ts
+++ frontend/src/app/pages/offer-list/offer-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { OfferListComponent } from './offer-list.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('OfferListComponent', () => {
+  let component: OfferListComponent;
+  let fixture: ComponentFixture<OfferListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [OfferListComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(OfferListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
