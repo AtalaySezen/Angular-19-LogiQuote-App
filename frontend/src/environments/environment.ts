352f9db08726d9291cad5a58eb8082578cf088a8
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-12-06 00:14:13 +0300

    changes : environment.ts and angular.json updated.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     appVersion: 'v1.0.0',
     APP_KEY: 'Logiquote',
-    apiUrl: ''
+    apiUrl: 'https://angular-19-logi-quote-app.vercel.app/api/'
 };
